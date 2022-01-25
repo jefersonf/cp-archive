45a50cda701c37b61ca8bc8f3503eff98f2f26c2
jeferson.silva <jeferson.silva@splab.ufcg.edu.br>
2022-01-25 11:04:54 -0300

    contest folder generator

---
+++ oj-tools/cc.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+contestID=$1
+tasks=$2
+dir=$3
+
+DEFAULT_CONTEST_ID="unknown"
+ALL_TASKS="ABCDE"
+DIR="."
+
+[ -z $contestID ] && contestID=$DEFAULT_CONTEST_ID
+[ -z $tasks ] && tasks=$ALL_TASKS
+[ -z $dir ] && dir=$DIR
+
+n=${#tasks}
+echo "Creating contest" $contestID \
+	"(tasks from ${tasks:0:1} to ${tasks:($n-1):$n})"
+
+contest_dir="${dir}/${contestID}"
+create_contest_folder=(mkdir -p $contest_dir)
+
+${create_contest_folder[@]}
+for t in $(seq 1 $n); do
+	task_file="${tasks:($t-1):1}.cpp"
+	task_path="${contest_dir}/${task_file}"
+	create_task=(cp ./template.cpp "${task_path}")
+	${create_task[@]}
+done
+
---
+++ oj-tools/template.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void opt() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+void solve(int tc) { 
+    //cout << "test case: " << tc << endl;
+}
+
+int main() {
+    opt();
+
+    freopen("tc.in", "r", stdin);
+    //freopen("tc.out", "w", stdout);
+
+    int tc=1;
+    //cin >> tc;
+    while (tc--)
+    solve(tc);
+    return 0;
+}
+
