e78e590ef6b485bfaac7cbd17d4fdca0e2cef020
Jeferson Vale <jefersonnpn@gmail.com>
2025-03-27 22:38:38 -0300

    :sparkles: shift rotation

--- CodeForces/cf1013/C/sol.cpp
+++ CodeForces/cf1013/C/sol.cpp
@@ -10,7 +10,8 @@ typedef long long ll;
 
 int main() {
     ios::sync_with_stdio(false); cin.tie(nullptr);
-    //freopen("in.txt", "r", stdin);
+    
+    freopen("in.txt", "r", stdin);
 
     int t; cin >> t;
     vector<int> a;
